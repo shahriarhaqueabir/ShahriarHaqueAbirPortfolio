349de747f1cd62af1811adadc75b720f5adc83eb
hawkward <shahriar_abby@hotmail.com>
2026-06-02 20:59:40 +0200

    seo

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
-  outputFileTracingRoot: path.join(__dirname, "../../"),
   images: {
     unoptimized: true,
   },
